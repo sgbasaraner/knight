af6c248002f47e7c27718e27ed49b2a19f801d01
Sarp Güney Başaraner <sarpbasaraner@gmail.com>
2018-10-25 18:27:27 +0300

    ensure correct usage

--- main.hs
+++ main.hs
@@ -1,5 +1,15 @@
 import System.Environment
+import Data.Char
+
+checkIfCorrectArgument :: String -> Bool
+checkIfCorrectArgument x =
+    (length x == 2) &&
+    toLower (head x) `elem` letters &&
+    head (tail x) `elem` numbers where
+        letters = ['a'..'h']
+        numbers = ['1'..'8']
+
 
 main = do
     args <- getArgs
-    putStrLn $ head args
+    putStrLn $ show (checkIfCorrectArgument (head args))
